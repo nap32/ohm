9826fc882048bcdc26ec6996de3f906c3de4dd8f
naptime <10912027+nap32@users.noreply.github.com>
2022-11-11 15:35:07 -0800

    Moved the response clone + response consuming function to prepare for next steps.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![allow(unused_imports)]
 #![allow(unused_variables)]
 #![allow(unused_mut)]
+#![allow(unused_assignments)]
 
 use std::{convert::Infallible, net::SocketAddr};
 use hyper::{Body, Request, Response, Server, Client, StatusCode};
@@ -16,25 +17,9 @@ use std::error::Error;
 // https://hyper.rs/guides/server/hello-world/
 // https://hyper.rs/guides/client/advanced/
 // https://tokio.rs/tokio/tutorial/async
+// https://github.com/hyperium/hyper/blob/master/examples/upgrades.rs
 //
 
-async fn handle(request: Request<Body>) -> Result<Response<Body>, Infallible> {
-    let response = send_request(request).await;
-    match response {
-        Ok(t) => {
-            let response = *t;
-            return Ok(response);
-        },
-        Err(e) => {
-            let body : Body = Body::empty();
-            let response = Response::builder()
-                .status(500)
-                .body(body)
-                .unwrap();
-            return Ok(response)
-        },
-    }
-}
 
 #[tokio::main]
 async fn main() { 
@@ -51,13 +36,57 @@ async fn main() {
     }
 }
 
+async fn handle(request: Request<Body>) -> Result<Response<Body>, Infallible> {
+    let result = send_request(request).await;
+    let mut response = Response::default();
+    let mut response_consumer = Response::default();
+    let mut response_provider = Response::default();
+
+    // Refactor for cleaner experience?
+    //if let Err(e) = result {
+    //    println!("Error:\n{}", e);
+    //} else if let Ok(t) = result{
+    //    response = *t;
+    //}
+
+    match result {
+        Ok(t) => {
+            response = *t;
+            let clone_result = clone_response(response).await;
+            match clone_result {
+                Ok(t) => {
+                    (response_consumer, response_provider) = t;
+                },
+                Err(e) => {
+                    println!("Error:\n{}", e);
+                },
+            }
+        },
+        Err(e) => {
+            println!("Error:\n{}", e);
+        },
+    }
+
+    let result_print = print_response(response_consumer).await;
+    match result_print {
+        Ok(t) => {
+        },
+        Err(e) => {
+            println!("Error:\n{}", e);
+        }
+    }
+
+    response = response_provider;
+    return Ok(response)
+}
+
 async fn send_request(request: Request<Body>) -> Result<Box<Response<Body>>, Box<dyn Error + Send + Sync>> {
     let https = HttpsConnector::new();
     let client = Client::builder().build::<_, hyper::Body>(https);
 
-    //let uri = "https://www.chess.com".parse()?;
-    //let mut resp = client.get(uri).await?;
-    println!("Request:\nURI: {}", request.uri());
+    println!("Request:");
+    println!("Method: {}", request.method());
+    println!("URI: {}", request.uri());
     println!("Headers: {:#?}", request.headers());
 
     let mut result = client.request(request).await;
@@ -71,20 +100,39 @@ async fn send_request(request: Request<Body>) -> Result<Box<Response<Body>>, Box
         }
     }
 
+    println!("Response:");
+    println!("Method: {}", response.status());
+    println!("Headers: {:#?}", response.headers());
+
     let body_bytes = hyper::body::to_bytes(response.into_body()).await?;
-    let mut response_foo = Response::builder()
+    let mut response_provider = Response::builder()
         .body(Body::from(body_bytes.clone()))?;
-    let mut response_bar = Response::builder()
+    let mut response_consumer = Response::builder()
         .body(Body::from(body_bytes.clone()))?;
 
-    while let Some(chunk) = response_foo.body_mut().data().await {
+    while let Some(chunk) = response_consumer.body_mut().data().await {
         stdout().write_all(&chunk?).await?;
     }
-    
-    response = response_bar;
-    println!("Response: {}", response.status());
 
+    let response = response_provider;
     let b = Box::new(response);
 
     Ok(b)
 }
+
+async fn clone_response(response: Response<Body>) -> Result<(Response<Body>, Response<Body>), Box<dyn Error + Send + Sync>> {
+    let body_bytes = hyper::body::to_bytes(response.into_body()).await?;
+    let mut response_provider = Response::builder()
+        .body(Body::from(body_bytes.clone()))?;
+    let mut response_consumer = Response::builder()
+        .body(Body::from(body_bytes.clone()))?;
+    return Ok((response_consumer, response_provider))
+}
+
+async fn print_response(mut response: Response<Body>) -> Result<(), Box<dyn Error + Send + Sync>> {
+    while let Some(chunk) = response.body_mut().data().await {
+        stdout().write_all(&chunk?).await?;
+    }
+    return Ok(())
+}
+
