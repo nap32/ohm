10b0e5fc633597df96d8a975d2189e212b92bb4e
naptime <10912027+nap32@users.noreply.github.com>
2022-11-22 16:24:12 -0800

    Refactored Record to be a flat structure instead of having seperate Request and Response structs within. Record is dead, all hail (new) Record. hyper::body::to_bytes() presents async call from sync function problem - to be fixed later.

--- src/record/mod.rs
+++ src/record/mod.rs
@@ -22,127 +22,64 @@ mod record {
     // impl Eq for N {}
     // impl N {}
 
-///
-/// REQUEST
-///
-    
     #[derive(Serialize, Deserialize, Debug, Clone)]
-    pub struct RequestRecord {
+    pub struct Record {
         pub method : String,
         pub scheme : String,
         pub host : String,
         pub path : String,
         pub query : String,
-        pub headers : HashMap<String, String>, // std::collections::HashMap;
-        pub body : String, // Is this best as String or Vec<u8> byte-string?
+        pub request_headers : HashMap<String, String>, 
+        pub request_body : Vec<u8>,
+        pub status: String,
+        pub response_headers : HashMap<String, String>, 
+        pub response_body : Vec<u8>, 
     }
-    impl PartialEq for RequestRecord {
+    impl PartialEq for Record {
         fn eq(&self, other: &Self) -> bool {
             (self.method == other.method) &&
                 (self.scheme == other.scheme) &&
                 (self.host == other.host) &&
                 (self.path == other.path) &&
                 (self.query == other.query) &&
-                (self.headers == other.headers) &&
-                (self.body == other.body)
-        }
-    }
-    impl Eq for RequestRecord {}
-    impl RequestRecord {
-        pub fn new(request : hyper::Request<hyper::Body>) -> Self {
-            let (parts, _body) = request.into_parts();
-            let me = Self {
-                method : "GET".to_string(),
-                scheme : parts.uri.scheme().unwrap().to_string(),   // I think these .unwrap()'s are
-                host : parts.uri.host().unwrap().to_string(),       // are going to cause trouble.
-                path : parts.uri.path().to_string(),
-                query : parts.uri.query().unwrap().to_string(),
-                headers : HashMap::<std::string::String, std::string::String>::new(),
-                body : "Foobar".to_string(),
-            };
-            return me;
-        }
-        pub fn get_uri(&self) -> std::string::String {
-            let uri = format!("{}://{}/{}", self.scheme, self.host, self.path); 
-            return uri
-        }
-        pub fn get_json(&self) -> std::string::String {
-            let serialized = serde_json::to_string(&self).unwrap();
-            return serialized
-        }
-    }
-
-///
-/// RESPONSE
-///
-
-    #[derive(Serialize, Deserialize, Debug, Clone)]
-    pub struct ResponseRecord {
-        pub status: String,
-        pub headers : HashMap<String, String>, // std::collections::HashMap;
-        pub body : String, // Is this best as String or Vec<u8> byte-string?
-    }
-    impl PartialEq for ResponseRecord {
-        fn eq(&self, other: &Self) -> bool {
-            (self.status == other.status) &&
-                (self.headers == other.headers) &&
-                (self.body == other.body)
-        }
-    }
-    impl Eq for ResponseRecord {}
-    impl ResponseRecord {
-        pub fn new(response : hyper::Response<hyper::Body>) -> Self {
-            let mut me = Self {
-                status : response.status().to_string().clone(),
-                headers : HashMap::<std::string::String, std::string::String>::new(),
-                body : "Foobar".to_string(),
-            };
-            for (key, value) in response.headers() {
-                me.headers.insert(key.to_string(), std::string::String::from(value.to_str().unwrap()));
-            }
-            return me
-        }
-        pub fn get_json(&self) -> std::string::String {
-            let serialized = serde_json::to_string(&self).unwrap();
-            return serialized
-        }
-    }
-
-///
-/// RECORD
-///
-
-    #[derive(Serialize, Deserialize, Debug, Clone)]
-    pub struct Record {
-        pub uri : String,
-        pub request : record::RequestRecord,
-        pub response : record::ResponseRecord,
-    }
-    impl PartialEq for Record {
-        fn eq(&self, other: &Self) -> bool {
-            (self.uri == other.uri) &&
-                (self.request == other.request) &&
-                (self.response == other.response)
+                (self.request_headers == other.request_headers) &&
+                (self.request_body == other.request_body) &&
+                (self.status == other.status) &&
+                (self.response_headers == other.response_headers) &&
+                (self.response_body == other.response_body)
         }
     }
     impl Eq for Record {}
     impl Record {
         pub fn new(request : hyper::Request<hyper::Body>, response : hyper::Response<hyper::Body>) -> Self {
-            let ohm_request = record::RequestRecord::new(request);
-            let ohm_response = record::ResponseRecord::new(response);
-            let me = Self {
-                uri : ohm_request.get_uri(),
-                request : ohm_request,
-                response : ohm_response,
+            let mut me = Self {
+                method : "GET".to_string(),
+                scheme : request.uri().scheme().unwrap().to_string(),   // I think these .unwrap()'s are
+                host : request.uri().host().unwrap().to_string(),       // are going to cause trouble.
+                path : request.uri().path().to_string(),
+                query : request.uri().query().unwrap().to_string(),
+                request_headers : HashMap::<std::string::String, std::string::String>::new(),
+                request_body : Vec::<u8>::new(),
+                status : response.status().to_string().clone(),
+                response_headers : HashMap::<std::string::String, std::string::String>::new(),
+                response_body : Vec::<u8>::new(),
             };
+            for (key, value) in request.headers() {
+                me.request_headers.insert(key.to_string(), std::string::String::from(value.to_str().unwrap()));
+            }
+            for (key, value) in response.headers() {
+                me.response_headers.insert(key.to_string(), std::string::String::from(value.to_str().unwrap()));
+            }
+            //me.request_body = hyper::body::to_bytes(request.into_body()).await.unwrap().to_vec();
+            //me.response_body = hyper::body::to_bytes(request.into_body()).await.unwrap().to_vec();
             return me 
         }
         pub fn get_key(&self) -> std::string::String {
-            let key = format!("{}:{}:{}", self.request.method, self.request.host, self.request.path);
+            let key = format!("{}:{}:{}", self.method, self.host, self.path);
             return key
         }
         pub fn get_uri(&self) -> std::string::String {
-            return self.request.get_uri()
+            return format!("{}://{}/{}", self.scheme, self.host, self.path); 
         }
         pub fn get_json(&self) -> std::string::String {
             let serialized = serde_json::to_string(&self).unwrap();
