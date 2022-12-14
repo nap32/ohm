9552b65e7c472bf2b5871c9e61c21bcb761623af
naptime <10912027+nap32@users.noreply.github.com>
2022-12-14 08:36:07 -0800

    Updating data layer.

--- src/datastore/mod.rs
+++ src/datastore/mod.rs
@@ -10,6 +10,11 @@ pub mod datastore {
     use crate::record::record;
     use crate::Record;
 
+    const DB_URL : &str = "mongodb://localhost:27017";
+    const APP_NAME : &str = "Ohm";
+    const DB_NAME : &str = "records";
+    const COLLECTION_NAME : &str = "records";
+
     // Manage and store all datastore interactions.
     pub struct Mongo {
         connection : mongodb::Client,
@@ -33,16 +38,16 @@ pub mod datastore {
 
         async fn get_connection() -> Result<mongodb::Client, mongodb::error::Error>{
             // Parse a connection string into an options struct.
-            let mut client_options = ClientOptions::parse("mongodb://localhost:27017").await?;
+            let mut client_options = ClientOptions::parse(DB_URL).await?;
 
             // Manually set an option.
-            client_options.app_name = Some("ohm".to_string());
+            client_options.app_name = Some(APP_NAME.to_string());
 
             let client = Client::with_options(client_options)?;
 
-            for db_name in client.list_database_names(None, None).await? {
-                println!("{}", db_name);
-            };
+            //for db_name in client.list_database_names(None, None).await? {
+            //    println!("{}", db_name);
+            //};
 
             Ok(client)
         }
@@ -53,17 +58,17 @@ pub mod datastore {
             let mut db : Option<mongodb::Database> = None;  
             let mut dbs = HashMap::<String, Option<mongodb::Database>>::new();
             for db_name in client.list_database_names(None, None).await? {
-                println!("{}", db_name);
+                //println!("{}", db_name);
                 dbs.insert(db_name, None);
             };
 
             // Check for 'records' DB, create if missing.
-            if dbs.contains_key("records") && dbs.get("records").is_some() {
-                db = Some(client.database("records")); 
+            if dbs.contains_key(DB_NAME) && dbs.get(DB_NAME).is_some() {
+                db = Some(client.database(DB_NAME)); 
             } else {
                 // I think you can create a database with the same verbiage,
                 // so this block is redundant.
-                db = Some(client.database("records"));
+                db = Some(client.database(DB_NAME));
             }
             
             // If no value is here will throw an error via .unwrap(), and return E instead of V.
@@ -74,62 +79,56 @@ pub mod datastore {
             let filter = doc!{ };
             let mut collection : Option<mongodb::Collection<Record>> = None; 
             let collection_names = db.list_collection_names(filter).await?;
-            for name in &collection_names {
-                println!("{}", name);
-            }
+            //for name in &collection_names {
+            //    println!("{}", name);
+            //}
 
-            let collection_name : String = "records".to_string();
-            if collection_names.contains(&collection_name) {
-               collection = Some(db.collection::<Record>("records"));
+            if collection_names.contains(&COLLECTION_NAME.to_string()) {
+               collection = Some(db.collection::<Record>(COLLECTION_NAME));
             } else {
                 // Note mongoDB creates collections implicitly when data is inserted,
                 // so this method is not needed if no special ops are required.
-                db.create_collection(&collection_name, None).await?;
-                collection = Some(db.collection::<Record>("records"));
+                db.create_collection(COLLECTION_NAME, None).await?;
+                collection = Some(db.collection::<Record>(COLLECTION_NAME));
 
             }
             Ok(collection.unwrap())
         }
 
-        pub async fn write_records(&mut self, records: Vec<Record>) -> Result<(), mongodb::error::Error> {
+        pub async fn add_records(&mut self, records: Vec<Record>) -> Result<(), mongodb::error::Error> {
             self.record_collection.insert_many(records, None).await?;
             Ok(())
         }
 
-        pub async fn read_records(&mut self) -> Result<(), mongodb::error::Error> {
+        pub async fn get_records(&mut self, record_filter : mongodb::bson::Document) -> Result<Vec::<Record>, mongodb::error::Error> {
 
-            let filter = doc!{ "host" : "foo.bar" };
-            let find_options = FindOptions::builder().sort(doc!{ "method" : 1 }).build();
-            let mut cursor = self.record_collection.find(filter, find_options).await?;
+            let mut results = Vec::<Record>::new();
+            //let find_options = FindOptions::builder().sort(doc!{ "method" : 1 }).build();
+            let mut cursor = self.record_collection.find(record_filter, None).await?;
 
             while let Some(record) = cursor.try_next().await? {
-                println!("method: {}", record.method);
-                println!("host: {}", record.host);
-                println!("path: {}", record.path);
+                results.push(record);
             }
 
+            Ok(results)
+        }
+
+        pub async fn update_records(&mut self, record_filter : mongodb::bson::Document, updates_filter : mongodb::bson::Document) -> Result<(), mongodb::error::Error> {
+            self.record_collection.update_many(record_filter, updates_filter, None).await?;
             Ok(())
         }
 
-        pub async fn update_records(&mut self) -> Result<(), mongodb::error::Error> {
-            let filter = doc!{ "host" : "foo.bar" };
-            let updates = doc!{ "$set" : { "host" : "bar.foo" } };
-            self.record_collection.update_many(filter, updates, None).await?;
+        pub async fn delete_records(&mut self, record_filter : mongodb::bson::Document) -> Result<(), mongodb::error::Error> {
+            self.record_collection.delete_many(record_filter, None).await?;
             Ok(())
         }
 
-        pub async fn delete_records(&mut self) -> Result<(), mongodb::error::Error> {
-            let filter = doc!{ "host" : "bar.foo" };
-            self.record_collection.delete_many(filter, None).await?;
-            Ok(())
-        }
-
-        pub async fn drop_collection(&mut self) -> Result<(), mongodb::error::Error> {
+        async fn drop_collection(&mut self) -> Result<(), mongodb::error::Error> {
             let _result = self.record_collection.drop(None).await?;
             Ok(())
         }
 
-        pub async fn drop_database(&mut self) -> Result<(), mongodb::error::Error> {
+        async fn drop_database(&mut self) -> Result<(), mongodb::error::Error> {
             let _options = mongodb::options::DropDatabaseOptions::builder().build();
             let _result = self.db.drop(None).await?;
             Ok(())
